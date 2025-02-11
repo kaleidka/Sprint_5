4a2957d06d465074b7d8d3e83fe1c46ca9d01cac
Gleb <glebkharitonenkov@gmail.com>
2025-02-11 21:38:22 +0300

    создал отдельный класс basepage и перенес туда методы из constructor

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,22 +1,7 @@
-from data import ACTIVE
+from base_page import BasePage
 from locators import Locators as loc
 
-class TestConstructor:
-
-    def click_element(self, driver, element_locator):
-        driver.find_element(*element_locator).click()
-
-    def get_element_attribute(self, driver, element_locator, attribute):
-        return driver.find_element(*element_locator).get_attribute(attribute)
-
-    def check_active_category(self, driver, active_locator, inactive_locators):
-        active_class = self.get_element_attribute(driver, active_locator, 'class')
-        assert ACTIVE in active_class
-
-        for locator in inactive_locators:
-            inactive_class = self.get_element_attribute(driver, locator, 'class')
-            assert ACTIVE not in inactive_class
-
+class TestConstructor(BasePage):
     def test_switch_to_buns_in_constructor(self, driver):
         self.click_element(driver, loc.SAUCES)
         self.click_element(driver, loc.BUNS)
